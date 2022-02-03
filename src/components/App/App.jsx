fb4543e5501bd4874030bcd90725409b815bf408
Nick Zahnow <nzahnow@gmail.com>
2022-02-03 09:55:20 -0600

    got /get to pizzas working and sending to redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+
+
 
 function App() {
+  const dispatch = useDispatch();
+  function fetchPizzas() {
+    axios.get('/api/pizza')
+      .then((response) => {
+        console.log('Got pizzas: ', response.data);
+        dispatch({
+          type: 'REPLACE_PIZZAS',
+          payload: response.data
+        })
+      })
+      .catch((error) => {
+        console.log('Error getting pizzas:', error);
+
+      })
+  }
+
+  useEffect(() => {
+    fetchPizzas();
+  }, []);
 
   return (
     <div className='App'>
@@ -11,6 +34,7 @@ function App() {
       </header>
   
       <img src='images/pizza_photo.png' />
+
       <p>Pizza is great.</p>
   
     </div>
--- src/index.js
+++ src/index.js
@@ -3,4 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
+
+const pizzaReducer = (state = [], action) => {
+    if (action.type === 'REPLACE_PIZZAS') {
+      return action.payload;
+    } 
+    return state;
+  }
+
+const orderReducer = (state = {}, action) => {
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+      pizzaReducer,
+      orderReducer
+    }));
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root'));
